c8ba9e43f0dd8a4065b40dcd909dace190e18935
winwinstar <451219599@qq.com>
2017-02-09 17:50:47 +0800

    update

--- src/main/webapp/html/js/getData.js
+++ src/main/webapp/html/js/getData.js
@@ -1,91 +1,182 @@
 $(document).ready(
 	function(){
 		$("#WIPByLine").click(function(){
-			var url = 'http://192.168.30.106:8080/showdata/ExposeData?getDataWay=WIPByLine';  
-			$.ajax(url, {    
+			var url = 'http://192.168.30.106:8080/showdata/ExposeData?getDataWay=WIPByLine';
+			$.ajax(url, {
 				crossDomain: true,
 				type:'get',
-				success: function(data) {  
-					var jsonobj = eval(data);
+				success: function(data) {
+					var jsonobj = eval(data);//通过eval() 函数可以将JSON字符串转化为对象
 					var data1=[];
 					var data2=[];
 					$.each(jsonobj, function (n, value) {
-					   data1.push(value.name);
-					   data2.push(value.num);
+						data1.push(value.name);
+						data2.push(value.num);
 					});
-				  
 
-					var myChart = echarts.init(document.getElementById('main'));	
-					
+					// 基于准备好的dom，初始化echarts实例
+					var myChart = echarts.init(document.getElementById('main'));
+					var colors = ['#5793f3', '#d14a61', '#675bba'];
 
-					var option = {
+					option = {
+						color: colors,
+
 						title: {
-							text: 'ECharts'
+							text: '图表管理'
 						},
-						tooltip: {},
+						tooltip: {
+							trigger: 'axis'
+						},
+						grid: {
+							left: '10%',
+							top: '20%',
+							right: '10%',
+							bottom: '15%'
+						},
+						toolbox: {
+							feature: {
+								dataView: {show: true, readOnly: false},
+								restore: {show: true},
+								saveAsImage: {show: true}
+							}
+						},
 						legend: {
-							data:['数量']
+							data:['ACTUALQTY','LINE']
 						},
-						xAxis: {
-							data: data1
-						},
-						yAxis: {},
-						series: [{
-							name: '数量',
-							type: 'bar',
-							data: data2
-						}]
+						xAxis: [
+							{
+								type: 'category',
+								axisTick: {
+									alignWithLabel: true
+								},
+								data: data1,
+								axisLabel:{
+									interval: 0
+								}
+							}
+						],
+						yAxis: [
+							{
+								show:false,
+							},
+							{
+								show:false,
+							},
+							{
+								type: 'value',
+								name: 'ACTUALQTY',
+								min: 0,
+								position: 'left',
+								axisLine: {
+									lineStyle: {
+										color: colors[2]
+									}
+								},
+								axisLabel: {
+									formatter: '{value}'
+								}
+							}
+						],
+						dataZoom: [
+							{
+								type: 'inside',
+								start: 0,
+								end: 100
+							},
+							{
+								show: true,
+								height: 20,
+								type: 'slider',
+								top: '90%',
+								xAxisIndex: [0],
+								start: 0,
+								end: 100
+							}
+						],
+						series: [
+							{
+								name:'ACTUALQTY',
+								type:'bar',
+								data:data2
+							},
+							{
+								name:'LINE',
+								type:'line',
+								yAxisIndex: 2,
+								data:data2
+							}
+						]
 					};
 
-
-					myChart.setOption(option);  
-				}  
+					// 使用刚指定的配置项和数据显示图表。
+					myChart.setOption(option);
+				}
 			});
 		});
-		
+
 		$("#WIPByProject").click(function(){
-			var url = 'http://192.168.30.106:8080/showdata/ExposeData?getDataWay=WIPByProject';  
-			$.ajax(url, {    
+			var url = 'http://192.168.30.106:8080/showdata/ExposeData?getDataWay=WIPByProject';
+			$.ajax(url, {
 				crossDomain: true,
 				type:'get',
-				success: function(data) {  
-					var jsonobj = eval(data);
+				success: function(data) {
+					var jsonobj = eval(data);//通过eval() 函数可以将JSON字符串转化为对象
 					var data1=[];
 					var data2=[];
 					$.each(jsonobj, function (n, value) {
-					   data1.push(value.name);
-					   data2.push(value.num);
+						data1.push(value.name);
+						data2.push(value.num);
 					});
-				  
 
-					var myChart = echarts.init(document.getElementById('main'));	
-					
+					// 基于准备好的dom，初始化echarts实例
+					var myChart = echarts.init(document.getElementById('main'));
 
+					// 指定图表的配置项和数据
 					var option = {
 						title: {
-							text: 'ECharts '
+							text: '图表管理'
 						},
 						tooltip: {},
+						grid: {
+							left: '10%',
+							top: '20%',
+							right: '10%',
+							bottom: '15%'
+						},
 						legend: {
-							data:['数量']
+							data:['ACQUALQTY']
 						},
 						xAxis: {
 							data: data1
 						},
 						yAxis: {},
+						dataZoom: [
+							{
+								type: 'inside',
+								start: 0,
+								end: 100
+							},
+							{
+								show: true,
+								height: 20,
+								type: 'slider',
+								top: '90%',
+								xAxisIndex: [0],
+								start: 0,
+								end: 100
+							}
+						],
 						series: [{
-							name: '数量',
+							name: 'ACQUALQTY',
 							type: 'bar',
 							data: data2
 						}]
 					};
 
-					myChart.setOption(option);  
-				}  
+					// 使用刚指定的配置项和数据显示图表。
+					myChart.setOption(option);
+				}
 			});
 		});
 	}
 );
-		
-		
-
